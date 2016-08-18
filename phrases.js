ed402692e75cce655fa244e90d8af59234fa9dc3
Chieferson <chieferson@gmx.de>
2016-08-18 14:03:03 +0200

    edited phrases formatting to be imported

--- phrases.js
+++ phrases.js
@@ -3,38 +3,33 @@ module.exports = {
     'Viel zu lernen du noch hast junger Padawan!',
     'Ich spreche nur meine Sprache und selbst damit hab ich schon Probleme!'
   ],
-  const init = [
+  init: [
     'Hey, ein neuer Tag ein neues Lunch Roulet. Wer ist dabei?' ,
     'Mann ich hab jetzt schon Hunger wie ein Bär nach dem Winterschlaf. Die Kugel im Lunchroulet rollt... wer ist dabei?' ,
     'Und täglich grüßt das Murmeltier... ääh... Lunch Roulette. Wer macht mit?'
-  ]
-
-  const join = [
+  ],
+  join: [
     'Herzlichen Glückwunsch... du bist im Recall' ,
     'Vergiss niemals: Aus großer Macht, folgt große Verantwortung! Du bist dabei :-)' ,
     'Guten Morgen, du bist dabei... Oh, und falls wir uns nicht mehr sehen, guten Tag, guten Abend und gute Nacht!' ,
     'Warum denn so ernst? Na gut das kann ich auch: Hiermit teilen ich ihnen offiziell mit, das sie am Lunch Roulette teilnehmen.',
     'Lord Vader... äääh #{res.user.name}, euer Besuch ehrt uns! Welch unerwartetes Vergnügen.'
-  ]
-
-  const leave = [
+  ],
+  leave: [
     'Oh wie schade... ich habe dann heute leider keine Rose für dich!' ,
     'Digga... du bist raus!' ,
     'Ok, du bist raus, aber hüte dich vor der dunklen Seite der Macht.' ,
     'Es gibt Gute Zeiten und Schlechte Zeiten... diesmal sind es bei dir schlechte Zeiten' ,
     'Was ist denn mit Dir heute? Therapie schlecht gelaufen?' ,
     'Na gut, aber eigentlich gilt hier Regel 3: Komm nie auf die unendlich dumme Idee weg zu laufen, denn ich hab 6 kleine Freunde, die alle schneller laufen können als du...'
-  ]
-
-  const status = [
+  ],
+  status: [
     'Alles läuft nach Wunsch. Wir sind N Luncher und es sind noch X Minuten bis zur Auslosung.'
-  ]
-
-  const reminder = [
+  ],
+  reminder: [
     'Alles was einen Anfang hat, hat auch ein Ende! In kürze beginnt die Lunch Roulette Auslosung. Letzte Chance mitzumachen...'
-  ]
-
-  const roulette = [
+  ],
+  roulette: [
     'Rien ne va plus... Nichts geht mehr! Die Auslosung wird gestartet...' ,
     'Die Jury zieht sich nun zur Auslosung zurück... Wir gehen in der Zwischenzeit in die Werbung...'
   ]
